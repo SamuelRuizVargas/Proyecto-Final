e488e43fee39d3a6d2b60a6252a5de65542b077e
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-10-19 15:28:10 -0500

    Creada la clase personaje

---
+++ proyecto_final/personaje.cpp
@@ -0,0 +1,6 @@
+#include "personaje.h"
+
+personaje::personaje()
+{
+
+}
---
+++ proyecto_final/personaje.h
@@ -0,0 +1,11 @@
+#ifndef PERSONAJE_H
+#define PERSONAJE_H
+
+
+class personaje
+{
+public:
+    personaje();
+};
+
+#endif // PERSONAJE_H
