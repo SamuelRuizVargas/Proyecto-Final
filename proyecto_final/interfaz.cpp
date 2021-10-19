6583b0d0d8af084bd54958f7e0d4eb10a37ce29c
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-10-19 12:37:36 -0500

    Primeras pruebas de cambio de escena y lvl 1

--- proyecto_final/interfaz.cpp
+++ proyecto_final/interfaz.cpp
@@ -1,29 +1,30 @@
 #include "interfaz.h"
 #include "ui_interfaz.h"
 
-
-
 Interfaz::Interfaz(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Interfaz)
 {
     ui->setupUi(this);
 
-    //Se crea la escena
+    //----------Escenas----------
+            //Principal
     scene = new QGraphicsScene;
-
     scene->setSceneRect(0,0,1260,650);
+            //Nivel 1
+    level_one = new QGraphicsScene;
+            //Nivel 2
+    level_two = new QGraphicsScene;
+            //Nivel 3
+    level_three = new QGraphicsScene;
+            //BossFight
+    bossfight = new QGraphicsScene;
+    //----------------------------
 
-    //dibujos
-    dibujarMenu();
-
-    //mostrar escena
-    ui->graphicsView->setScene(scene);
-    ui->graphicsView->show();
-
-
-
-
+    //----Crear objetos en escenas-----
+    crearMenu();
+    crearLevelOne();
+    //---------------------------------
 }
 
 Interfaz::~Interfaz()
@@ -31,10 +32,9 @@ Interfaz::~Interfaz()
     delete ui;
 }
 
-//Creación de niveles
-void Interfaz::dibujarMenu()
+void Interfaz::crearMenu()//Crea y agrega los elementos del menu inicial
 {
-    //Imagenes
+    //-------------------Imagenes---------------
     menu.append(new Imagenes(-11,0,1281,651,2)); // Background
     menu.append(new Imagenes(275,-100,700,400,1)); // Logo
 
@@ -45,7 +45,9 @@ void Interfaz::dibujarMenu()
         scene->addItem(menu.at(cont));
         cont++;
     }
-    //Botones
+    //------------------------------------------
+
+    //------------------Botones-----------------
     buttons.append(new botones(525,275,225,75,1));
     buttons.append(new botones(525,350,225,75,2));
 
@@ -56,33 +58,73 @@ void Interfaz::dibujarMenu()
         scene->addItem(buttons.at(cont));
         cont++;
     }
+    //------------------------------------------
+    //Set Scene para el menu inicial
+    ui->graphicsView->setScene(scene);
 }
 
-void Interfaz::mousePressEvent(QMouseEvent *event)
+void Interfaz::crearLevelOne()//Crea y agrega los elementos del nivel 1
 {
+    //-----------------Se saca la base del mapa------------------
+    ifstream archivo;
+    string coorde,numero,int1,int2,int3,int4,digi;
+    int ente1,ente2,ente3,ente4,len,conta;
+    archivo.open(PATH_BASE_LVL1, ios::in);
+    while(!archivo.eof())
+    {
+        if (archivo.eof())
+            break;
+        getline(archivo,coorde);
+        len=coorde.length();
+        conta=0;
+        for (int i=0; i<=len;i++)
+        {
+            digi=coorde[i];
+            if (digi!="," and digi[0]!='\000' )
+            {
+                numero+=digi;
+            }
+            else
+            {
+                conta+=1;
+                if(conta==1)
+                    int1+=numero;
+                else if(conta==2)
+                    int2+=numero;
+                else if(conta==3)
+                    int3+=numero;
+                else if(conta==4)
+                    int4+=numero;
+                numero.erase();
+            }
+        }
+        ente1=atoi(int1.c_str());
+        ente2=atoi(int2.c_str());
+        ente3=atoi(int3.c_str());
+        ente4=atoi(int4.c_str());
+        int1.erase();
+        int2.erase();
+        int3.erase();
+        int4.erase();
+        base_lvl1.append(new plataforma(ente1,ente2,ente3,ente4));
+        level_one->addItem(base_lvl1.back());
+    }
+    archivo.close();
+    //------------------------------------------------------------
+}
 
+void Interfaz::mousePressEvent(QMouseEvent *event)//Evento de clic con mouse
+{
+    //Si se da click verificar si fue en algun boton (Esto para hacer las pruebas de los mapas)
     QList<botones*>::iterator ite;
     int cont =0;
     for(ite = buttons.begin(); ite!= buttons.end(); ite++)
     {
-    bool Press = buttons.at(cont)->get_Pressed();
-    if(Press){
-       Level_one();
+        bool Press = buttons.at(cont)->get_Pressed();
+        if(Press)
+        {
+           ui->graphicsView->setScene(level_one);//cambio de escena para probar el lvl 1
+        }
+        cont++;
     }
-
-    cont++;
-   }
-
-}
-
-void Interfaz::Level_one()
-{
-    ui->graphicsView->hide();
-    ui->pushButton->hide();
-    ui->pushButton_2->hide();
-
-
-    level_one = new QGraphicsScene;
-    ui->graphicsView->setScene(level_one);
-    ui->graphicsView->show();
 }
--- proyecto_final/interfaz.h
+++ proyecto_final/interfaz.h
@@ -3,12 +3,19 @@
 
 #include <QGraphicsScene>
 #include <QMainWindow>
+#include <fstream>
+#include <QMovie>//REVISAR (para poner videitos supongo)
 #include <QList>
-#include <QMovie>
 
 #include "imagenes.h"
 #include "botones.h"
-#include "niveluno.h"
+#include "plataforma.h"
+
+using namespace std;
+
+//-----------Rutas nivel 1--------------
+#define PATH_BASE_LVL1 "../proyecto_final/posiciones/lvl1/posi_base_lvl1.txt"
+//--------------------------------------
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Interfaz; }
@@ -20,20 +27,39 @@ class Interfaz : public QMainWindow
 
 public:
     Interfaz(QWidget *parent = nullptr);
-
-    void Level_one();
     ~Interfaz();
 
 private:
     Ui::Interfaz *ui;
 
+    //---------Escenas----------
     QGraphicsScene *scene;
     QGraphicsScene *level_one;
+    QGraphicsScene *level_two;
+    QGraphicsScene *level_three;
+    QGraphicsScene *bossfight;
+    //--------------------------
 
+    //-----------Listas---------
+                //MENU
     QList<Imagenes*> menu;
     QList<botones*> buttons;
+                //LVL 1
+    QList<plataforma*> base_lvl1;
+                //LVL 2
+                //LVL 3
+                //BOSSFIGHT
+    //--------------------------
 
-    void dibujarMenu();
+    //----------Metodos---------
+                //MENU
+    void crearMenu();
+                //LVL 1
+    void crearLevelOne();
+                //LVL 2
+                //LVL 3
+                //BOSSFIGHT
+    //--------------------------
 
 protected:
     void mousePressEvent(QMouseEvent *event);
