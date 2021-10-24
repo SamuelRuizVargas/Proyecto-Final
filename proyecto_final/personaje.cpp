7d6d96cee43a8cf0d7b82d75ddc141ff9430838a
Samuel Ruiz Vargas <samuel.ruizv@udea.edu.co>
2021-10-24 14:03:07 -0500

    Actualizado personaje

--- proyecto_final/personaje.cpp
+++ proyecto_final/personaje.cpp
@@ -12,7 +12,6 @@ personaje::~personaje()
 
 }
 
-
 QRectF personaje::boundingRect() const
 {
     return QRectF(0, 0, an, h);
@@ -58,7 +57,6 @@ void personaje::setVY(float velocidady)
 
 void personaje::free(float dt)
 {
-
     posx=posx+vx*dt;
     posy-=vy2*dt+(g*dt*dt)/2.0f;
     vy2=vy2+g*dt;
